871820825b5871939d4d66ce21f01dcf430f4c48
Leestan360 <stanleymayore2@gmail.com>
2022-09-16 08:31:06 +0300

    Style the users

--- src/components/User.js
+++ src/components/User.js
@@ -3,10 +3,10 @@ import React from "react";
 function User({ users }) {
 
   return (
-    <div className="border-2 rounded-sm m-2 text-xl p-2 cursor-pointer">
+    <div className="m-2 text-xl p-2 cursor-pointer grid gap-3 grid-cols-4 grid-rows-3 mx-12 my-28">
       {users.map((user) => {
         return (
-          <div key={user.user_id} className="border-2 my-2 grid gap-3">
+          <div key={user.user_id} className="border-2 rounded-lg p-2 my-2 border-blue-400 shadow-xl hover:bg-blue-400">
             <p>Name: {user.name}</p>
           </div>
         );
